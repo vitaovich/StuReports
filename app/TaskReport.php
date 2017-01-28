9b7bdb85088e936c4582454e19d7f0b908b6789c
mike <mcollison@eagles.ewu.edu>
2017-01-27 17:12:16 -0800

    Individual Report now shows all prior tasks

---
+++ app/TaskReport.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TaskReport extends Model
+{
+  protected $table = 'task_reports';
+  protected $fillable = array(
+    'Individual_Report_id',
+    'Latest_Progress',
+    'Task_id',
+    'Sprint',
+    'Remaining_Sprints',
+    'Reassigned',
+  );
+}
