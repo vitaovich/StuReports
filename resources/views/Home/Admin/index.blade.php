4b247d9171a7326d198ea484f0379394599be10a
vitaovich <vitaliyalekhnovich@gmail.com>
2017-01-30 13:08:12 -0800

    fixed styling of home pages for roles

--- resources/views/Home/Admin/index.blade.php
+++ resources/views/Home/Admin/index.blade.php
@@ -6,10 +6,8 @@
   <div class="row">
       <div class="col-md-8 col-md-offset-2">
           <div class="panel panel-default">
-              <div class="panel-heading">Admin Home page
-                <a class="btn-sm btn-primary" href="/course/create">Create Course</a>
+              <div class="panel-heading"><h1>Admin Home page</h1>
               </div>
-              <div class="container">
               <div class="panel-body">
                 @if(Auth::user()->isAdmin())
                 <table class="table table-striped">
@@ -17,21 +15,20 @@
                     <tr>
                       <th>Course</th>
                       <th>Instructor</th>
-                      <th><th>
+                      <th><a class="btn-sm btn-success" href="/course/create">Create Course</a></th>
                     </tr>
                   </thead>
                   <tbody>
                     @foreach (Auth::user()->classrooms() as $classroom)
                       <tr>
                         <td>CSCD {{$classroom->Quarter}} {{$classroom->Year}}</td>
-                        <td>Instructor: {{$classroom->instructor->name}}</td>
+                        <td>{{$classroom->instructor->name}}</td>
                         <td><a href="/course/{{$classroom->Class_id}}/edit" class="btn-sm btn-primary">Edit</a></td>
                       </tr>
                     @endforeach
                   </tbody>
                 </table>
                 @endif
-              </div>
             </div>
           </div>
       </div>
--- resources/views/Home/Instructor/index.blade.php
+++ resources/views/Home/Instructor/index.blade.php
@@ -7,20 +7,27 @@
       <div class="col-md-8 col-md-offset-2">
           <div class="panel panel-default">
             <div class="panel-heading"><h1>CSCD {{$course->Quarter}} {{$course->Year}}</h1></div>
-
             <div class="panel-body">
               @if(Auth::user()->isInstructor())
-                @foreach ($course->projects as $project)
-                  <div data-toggle="collapse" data-target="#project_{{$project->Group_id}}">
-
-                    <h4><span class="caret"></span> Group {{$project->Group_id}}: {{$project->Project}}</h4>
-                  </div>
-                  <div id="project_{{$project->Group_id}}" class="collapse container">
-                      @foreach ($project->student_group as $student)
-                      <p>{{$student->user->name}}</p>
-                      @endforeach
-                  </div>
-                @endforeach
+              <table class="table table-striped">
+                <tbody>
+                  @foreach ($course->projects as $project)
+                    <tr>
+                      <td><div data-toggle="collapse" data-target="#project_{{$project->Group_id}}">
+                        <h4><span class="caret"></span> Group {{$project->Group_id}}: {{$project->Project}}</h4>
+                      </div>
+                      <div id="project_{{$project->Group_id}}" class="collapse">
+                        <ul>
+                          @foreach ($project->student_group as $student)
+                          <li>{{$student->user->name}}</li>
+                          @endforeach
+                        </ul>
+                      </div>
+                    </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
               @endif
             </div>
           </div>
