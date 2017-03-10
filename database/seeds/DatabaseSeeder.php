38a40bc77f657a545b1a6b7d04c59d7499f7aaba
Seth Riedel <seth.riedel@gmail.com>
2017-03-09 20:20:21 -0800

    Removed seeders that try to seed tables that no longer exist

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -30,8 +30,6 @@ class DatabaseSeeder extends Seeder
       $this->call(UsersTableSeeder::class);
       $this->call(IndividualReportsSeeder::class);
       $this->call(TasksSeeder::class);
-      $this->call(AssignmentsSeeder::class);
-      $this->call(GradebookSeeder::class);
       $this->call(MemberEvaluationsSeeder::class);
       $this->call(TaskEvaluationsSeeder::class);
       $this->call(TaskReportsSeeder::class);
